ccc5dd80d7e5ec66e96b57acd9fa35600a14d08b
Juank2138 <111672285+Juank2138@users.noreply.github.com>
2023-11-23 19:01:38 -0500

    Update models.py

--- inventario/inventarioapp/models.py
+++ inventario/inventarioapp/models.py
@@ -187,22 +187,22 @@ print(almacen.obtener_producto(1))
 
 "Arbol"
 class Nodo:
-    def _init_(self, dato):
+    def _init_(self, Id):
         self.dato=dato
         self.arbol=["comida", "verdura", "fruta"]
         self.baseV=["verdura"]#Añadir id de cada verdura
         self.baseF=["fruta"]#Añadir id de cada fruta
       
-    def insertar(self, raiz, padre, nodo):
-        if raiz.dato==padre:
+    def insertar(self, raiz, categoria, nodo):
+        if raiz.dato==categoria:
             raiz.arbol.append(nodo)
         else:
             l=len(raiz.arbol)
             for i in range(l):
-                if raiz.arbol[i].dato==padre:
-                    insertar(raiz.arbol[i], padre, nodo)
+                if raiz.arbol[i].dato==categoria:
+                    insertar(raiz.arbol[i], categoria, nodo)
                 else:
-                    insertar(raiz.arbol[i], padre, nodo)
+                    insertar(raiz.arbol[i], categoria, nodo)
 
 
 """class Inventario:
